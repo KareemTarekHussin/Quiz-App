b92984369d7f90ea8f5bbe65d3bd449fbe68d5e6
KareemTarekHussin <kareemtarek3219521@gmail.com>
2024-06-27 02:23:07 +0300

    First commit

--- src/Modules/AuthenticationModule/components/ForgetPass/ForgetPass.tsx
+++ src/Modules/AuthenticationModule/components/ForgetPass/ForgetPass.tsx
@@ -1,6 +1,26 @@
+import logo from "../../../../assets/images/logo.png";
+import image from "../../../../assets/images/Image.png";
 
 export default function ForgetPass() {
   return (
-    <div>ForgetPass</div>
+    <div className="min-h-screen bg-[#0D1321] text-white">
+    <div className="container">
+      <div className="flex">
+        <div className="flex-1 h-screen pt-8">
+          <img src={logo} className="w-[150px]" alt="" />
+          <div className="mt-10">
+          <p className="text-[#C5D86D] text-2xl font-semibold">Forgot Password</p>
+
+        {/* input */}
+        {/* buton */}
+        {/* navigation to login */}
+          </div>
+        </div>
+        <div className="flex-1 hidden md:flex justify-end items-center h-screen">
+          <img src={image} className="w-[80%]" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
