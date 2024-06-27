1c92336597bc1d2315c27f8d8f4d60b059b0fd66
KareemTarekHussin <kareemtarek3219521@gmail.com>
2024-06-27 16:26:11 +0300

    Second Commit

--- src/Modules/AuthenticationModule/components/ForgetPass/ForgetPass.tsx
+++ src/Modules/AuthenticationModule/components/ForgetPass/ForgetPass.tsx
@@ -1,26 +1,61 @@
 import logo from "../../../../assets/images/logo.png";
 import image from "../../../../assets/images/Image.png";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import EmailIcon from '@mui/icons-material/Email';
+import { Link, useNavigate } from "react-router-dom";
 
 export default function ForgetPass() {
-  return (
-    <div className="min-h-screen bg-[#0D1321] text-white">
-    <div className="container">
-      <div className="flex">
-        <div className="flex-1 h-screen pt-8">
-          <img src={logo} className="w-[150px]" alt="" />
-          <div className="mt-10">
-          <p className="text-[#C5D86D] text-2xl font-semibold">Forgot Password</p>
+  const navigate = useNavigate();
 
-        {/* input */}
-        {/* buton */}
-        {/* navigation to login */}
+  return (
+    <div className="min-h-screen bg-[#0D1321] text-white font-nunito">
+      <div className="container">
+        <div className="flex">
+          <div className="flex-1 h-screen pt-8">
+            <img src={logo} className="w-[150px]" alt="" />
+            <div className="mt-10">
+              <p className="text-[#C5D86D] text-2xl font-semibold">
+                Forgot Password
+              </p>
+              <div>
+                <form className="space-y-4 font-[sans-serif] text-[#333] max-w-md mx-auto">
+                  <div className="relative flex items-center">
+                    <EmailIcon className="absolute left-4 text-[#F5F5F5]" />
+                    <input
+                      type="email"
+                      placeholder="Type Your email"
+                      className="pl-12 px-4 py-3 bg-[#0D1321] focus:bg-transparent w-full text-sm border outline-[#ccc] rounded-xl transition-all"
+                    />
+                  </div>
+                  <button
+                    type="button"
+                    className="px-6 py-2.5 !mt-8 font-nunito font-bold text-sm border bg-[#F5F5F5] hover:bg-[#000] text-black rounded-lg active:bg-[#000]"
+                  >
+                    Send Email <CheckCircleIcon />
+                  </button>
+                </form>
+              </div>
+              {/* input */}
+              {/* button */}
+              {/* navigation to login */}
+              <div className="grid justify-items-end">
+                <div>
+                  Login?  
+                  <span className="text-[#C5D86D] font-semibold">
+                    <Link to="/login">
+                      Click here
+                    </Link>
+                  </span>
+                </div>
+              </div>
+              {/* navigation to login */}
+            </div>
           </div>
-        </div>
-        <div className="flex-1 hidden md:flex justify-end items-center h-screen">
-          <img src={image} className="w-[80%]" alt="" />
+          <div className="flex-1 hidden md:flex justify-end items-center h-screen">
+            <img src={image} className="w-[80%]" alt="" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
--- src/Modules/AuthenticationModule/components/ResetPass/ResetPass.tsx
+++ src/Modules/AuthenticationModule/components/ResetPass/ResetPass.tsx
@@ -1,6 +1,116 @@
+import logo from "../../../../assets/images/logo.png";
+import image from "../../../../assets/images/Image.png";
 
 export default function ResetPass() {
   return (
-    <div>ResetPass</div>
+    <div className="min-h-screen bg-[#0D1321] text-white">
+    <div className="container">
+      <div className="flex">
+        <div className="flex-1 h-screen pt-8">
+          <img src={logo} className="w-[150px]" alt="" />
+          <div className="mt-10">
+            <p className="text-[#C5D86D] text-2xl font-semibold">
+              Forgot Password
+            </p>
+            <div>
+              <form className="space-y-4 font-[sans-serif] text-[#333] max-w-md mx-auto">
+
+                <div className="relative flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#bbb"
+                    stroke="#bbb"
+                    className="w-[18px] h-[18px] absolute left-4"
+                    viewBox="0 0 682.667 682.667"
+                  >
+                    <defs>
+                      <clipPath id="a" clipPathUnits="userSpaceOnUse">
+                        <path
+                          d="M0 512h512V0H0Z"
+                          data-original="#000000"
+                        ></path>
+                      </clipPath>
+                    </defs>
+                    <g
+                      clip-path="url(#a)"
+                      transform="matrix(1.33 0 0 -1.33 0 682.667)"
+                    >
+                      <path
+                        fill="none"
+                        stroke-miterlimit="10"
+                        stroke-width="40"
+                        d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
+                        data-original="#000000"
+                      ></path>
+                      <path
+                        d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
+                        data-original="#000000"
+                      ></path>
+                    </g>
+                  </svg>
+                  <input
+                    type="email"
+                    placeholder="Type Your email"
+                    className="pl-12 px-4 py-3 bg-[#0D1321] focus:bg-transparent w-full text-sm border outline-[#0D1321] rounded transition-all"
+                  />
+                </div>
+                <div className="relative flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#bbb"
+                    stroke="#bbb"
+                    className="w-[18px] h-[18px] absolute left-4"
+                    viewBox="0 0 682.667 682.667"
+                  >
+                    <defs>
+                      <clipPath id="a" clipPathUnits="userSpaceOnUse">
+                        <path
+                          d="M0 512h512V0H0Z"
+                          data-original="#000000"
+                        ></path>
+                      </clipPath>
+                    </defs>
+                    <g
+                      clip-path="url(#a)"
+                      transform="matrix(1.33 0 0 -1.33 0 682.667)"
+                    >
+                      <path
+                        fill="none"
+                        stroke-miterlimit="10"
+                        stroke-width="40"
+                        d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
+                        data-original="#000000"
+                      ></path>
+                      <path
+                        d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
+                        data-original="#000000"
+                      ></path>
+                    </g>
+                  </svg>
+                  <input
+                    type="email"
+                    placeholder="Type Your email"
+                    className="pl-12 px-4 py-3 bg-[#0D1321] focus:bg-transparent w-full text-sm border outline-[#0D1321] rounded transition-all"
+                  />
+                </div>
+                <button
+                  type="button"
+                  className="px-6 py-2.5 !mt-8 text-sm bg-[#007bff] hover:bg-blue-600 text-white rounded active:bg-[#006bff]"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+            {/* input */}
+            {/* button */}
+            {/* navigation to login */}
+          </div>
+        </div>
+        <div className="flex-1 hidden md:flex justify-end items-center h-screen">
+          <img src={image} className="w-[80%]" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
